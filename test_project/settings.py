e34ac4d0b0c86ca0bd22704716561684216ecd5d
Muhammad Yasir <yasir.qadeer@devsinc.com>
2023-01-03 21:06:00 +0500

    Application - Django Secret key removed from setting - Added python-dotenv - Implemented env in settings.py

--- test_project/settings.py
+++ test_project/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+# Load envs
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@g#c^y74sy!r@wf7-r#73qanrxexu(e^_9a5=b29re4wh7)1eu'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
